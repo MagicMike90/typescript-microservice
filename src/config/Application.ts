6fe951954a346f1914f800b6c8126c4a882b07dd
Michael Luo <michael.luo0801@gmail.com>
2019-10-06 17:26:07 +1100

    project setup

--- src/config/Application.ts
+++ src/config/Application.ts
@@ -1,8 +1,8 @@
 import * as config from 'config';
 
-import { logger } from '../common/Logging';
 import { ExpressConfig } from './Express';
 
+// import { logger } from '../common/Logging';
 export class Application {
   server: any;
   express: ExpressConfig;
@@ -14,13 +14,13 @@ export class Application {
     const debugPort = config.get("express.debug");
 
     this.server = this.express.app.listen(port, () => {
-      logger.info(`
-      --------------------------------------------------
-       Server Started! Express: http://localhost:${port}
-       Health : http://localhost:${port}/ping
-       Debugger: http:/${this.server.address()}:${port}/?ws=${this.server.address()}:${port}&port=${debugPort}
-      ------------------------------------------------------
-      `);
+      // logger.info(`
+      // --------------------------------------------------
+      //  Server Started! Express: http://localhost:${port}
+      //  Health : http://localhost:${port}/ping
+      //  Debugger: http:/${this.server.address()}:${port}/?ws=${this.server.address()}:${port}&port=${debugPort}
+      // ------------------------------------------------------
+      // `);
     });
   }
 }
